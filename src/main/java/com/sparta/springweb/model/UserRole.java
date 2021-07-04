b94bb28f35ccd106d401e2bc940b17653adad15b
Gorani92 <peses0326@gmail.com>
2021-07-04 22:55:12 +0900

    회원가입, 관리자 권한 기능  todo : password db 암호화

---
+++ src/main/java/com/sparta/springweb/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.sparta.springweb.model;
+
+
+public enum UserRole {
+    USER,  // 사용자 권한
+    ADMIN  // 관리자 권한
+}
---
+++ src/main/java/com/sparta/springweb/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.springweb.repository;
+
+
+import com.sparta.springweb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
