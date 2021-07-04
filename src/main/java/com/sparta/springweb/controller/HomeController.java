bb9ab6e3435432abbd752b34467000af4c761029
Gorani92 <peses0326@gmail.com>
2021-07-05 03:40:31 +0900

    password db 암호화 / 메인에 사용자이름 표시, 로그아웃기능  todo : 삭제, 수정 버튼 구현하기

---
+++ src/main/java/com/sparta/springweb/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.sparta.springweb.controller;
+
+
+import com.sparta.springweb.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+// 홈으로 가는 controller : addAttribute 로 username 을 전달 해주고 있다.
+@Controller
+public class HomeController {
+    @GetMapping("/index.html") // security가 전달하는 userdetailsImpl 이다.
+    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
+        return "index";
+    }
+}
